2e0f4714fa1bf82d158bf0154e4876dede014485
Ajmal AJ <86530806+Ajmal-AJ@users.noreply.github.com>
2024-12-14 18:02:15 +0000

    cors header updation

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,7 +8,7 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 
