d8b6fd963473ac3b5981b34c6cf1f55ed2297148
Ajmal AJ <86530806+Ajmal-AJ@users.noreply.github.com>
2024-12-14 17:56:49 +0000

    cors header updation

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,11 +5,26 @@ import (
 	"strings"
 )
 
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("Authorization")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 
 		token := strings.TrimPrefix(tokenString, "Bearer ")
 
